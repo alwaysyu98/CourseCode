28f15c61ebd0b4660ea12ea02a6cf4c528710738
jiriyu <jiriyu98@gmail.com>
2022-12-21 21:29:59 +0900

    add: assignment3 all done

--- OpenCourse/IU_C311_Programming_Languages/solution/a3/a3.rkt
+++ OpenCourse/IU_C311_Programming_Languages/solution/a3/a3.rkt
@@ -11,24 +11,98 @@
             [`,boolean (guard (boolean? boolean)) `,boolean] ;booleans
             [`zero? `,(lambda (x) (zero? x))] ;zero?
             [`sub1 `,(lambda (x) (sub1 x))]
+            [`,var (guard (symbol? var)) `,(unbox (env var))] ;variables
             [`(* ,x ,y) `,(* (value-of x env) (value-of y env))] ; *
-            [`,var (guard (symbol? var)) `,(env var)] ;variables
             [`(if ,test ,res1 ,res2) `,(cond
                                          ((value-of test env) (value-of res1 env))
                                          (else (value-of res2 env)))] ;if
-            [`(lambda (,id) ,body) `,(lambda (arg) (value-of body (lambda (var)
-                                                                    (cond
-                                                                      ((eq? id var) arg)
-                                                                      (else (env var))))))] ;lambda
+            [`(lambda (,id) ,body) `,(lambda (arg) (value-of body
+                                                             (let ((bx (box arg)))
+                                                               (lambda (var)
+                                                                 (cond
+                                                                   ((eq? id var) bx)
+                                                                   (else (env var)))))))] ;lambda
             [`(,id ,body) `,((value-of id env) (value-of body env))] ;application
-            [`(let ([,arg ,def]) ,body) `,(value-of body (lambda (var)
-                                                           (cond
-                                                             ((eq? arg var) (value-of def env))
-                                                             (else (env var)))))] ; let
+            [`(let ([,arg ,def]) ,body) `,(value-of body
+                                                    (let ((bx (box (value-of def env))))
+                                                      (lambda (var)
+                                                        (cond
+                                                          ((eq? arg var) bx)
+                                                          (else (env var))))))] ; let
+            [`(set! ,x ,def) `,(set-box! (env x) (value-of def env))] ;set!
+            [`(begin2 ,x ,y) `,(begin (value-of x env) (value-of y env))] ;begin2
             )))
 
-; interpreter2 and interpreter3 are similiar
-; not diffcult and easy to understand, so I skipped :-)
+(value-of
+ '((lambda (x)
+     (begin2 (set! x 5) x))
+   6)
+ (lambda (y) (error 'value-of "unbound variable ~s" y)))
+
+; value-of-fn
+; treat env as a function
+(define empty-env-fn
+  (lambda ()
+    (lambda (var) (error "no var"))))
+(define extend-env-fn
+  (lambda (id arg env)
+    (lambda (var)
+      (cond
+        ((eq? id var) arg)
+        (else (env var))))))
+(define apply-env-fn
+  (lambda (env var)
+    (env var)))
+
+(define value-of-fn
+  (lambda (exp env)
+    (pmatch exp
+            [`,n (guard (integer? n)) `,n] ;numbers
+            [`,boolean (guard (boolean? boolean)) `,boolean] ;booleans
+            [`zero? `,(lambda (x) (zero? x))] ;zero?
+            [`sub1 `,(lambda (x) (sub1 x))] ;sub1
+            [`(* ,x ,y) `,(* (value-of-fn x env) (value-of-fn y env))] ; *
+            [`,var (guard (symbol? var)) `,(apply-env-fn env var)] ;variables
+            [`(if ,test ,res1 ,res2) `,(cond
+                                         ((value-of-fn test env) (value-of-fn res1 env))
+                                         (else (value-of-fn res2 env)))] ;if
+            [`(lambda (,id) ,body) `,(lambda (arg) (value-of-fn body (extend-env-fn id arg env)))] ;lambda
+            [`(,id ,body) `,((value-of-fn id env) (value-of-fn body env))] ;application
+            [`(let ([,id ,arg]) ,body) `,(value-of-fn body (extend-env-fn id (value-of-fn arg env) env))] ; let
+            )))
+
+; value-of-ds
+; treat env as a list
+(define empty-env-ds
+  (lambda ()
+    `(empty-env)))
+(define extend-env-ds
+  (lambda (id arg env)
+    `(extend-env-ds ,id ,arg, env)))
+(define apply-env-ds
+  (lambda (env var)
+    (pmatch env
+     [`(empty-env) `,(error "no var")]
+     [`(extend-env-ds ,id ,arg, env) (cond
+                                       ((eq? id var) arg)
+                                       (else (apply-env-ds env var)))])))
+
+(define value-of-ds
+  (lambda (exp env)
+    (pmatch exp
+            [`,n (guard (integer? n)) `,n] ;numbers
+            [`,boolean (guard (boolean? boolean)) `,boolean] ;booleans
+            [`zero? `,(lambda (x) (zero? x))] ;zero?
+            [`sub1 `,(lambda (x) (sub1 x))]
+            [`(* ,x ,y) `,(* (value-of-ds x env) (value-of-ds y env))] ; *
+            [`,var (guard (symbol? var)) `,(apply-env-ds env var)] ;variables
+            [`(if ,test ,res1 ,res2) `,(cond
+                                         ((value-of-ds test env) (value-of-ds res1 env))
+                                         (else (value-of-ds res2 env)))] ;if
+            [`(lambda (,id) ,body) `,(lambda (arg) (value-of-ds body (extend-env-ds id arg env)))] ;lambda
+            [`(,id ,body) `,((value-of-ds id env) (value-of-ds body env))] ;application
+            [`(let ([,id ,arg]) ,body) `,(value-of-ds body (extend-env-ds id (value-of-ds arg env) env))] ; let
+            )))
 
 ; Part2
 ; 4
@@ -59,11 +133,14 @@
             )))
 
 ; 6
+; there are some differences between 2013ver and 2022ver.
+; const and zero
+; but it doesn't matter, apply-env-lex and exten-env-lex are not changed 
 (define value-of-lex
   (lambda (exp env)
     (match exp
-      (`,b #:when (boolean? b) exp)
-      (`,n #:when (number? n) exp)
+      [`(const ,expr) expr]
+      [`(zero ,x) (zero? (value-of-lex x env))]
       (`(sub1 ,body) (sub1 (value-of-lex body env)))
       (`(zero? ,body) (zero? (value-of-lex body env)))
       (`(* ,n1 ,n2) (* (value-of-lex n1 env) (value-of-lex n2 env)))
